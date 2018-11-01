681af520166a532d1236ef71207c49cfcabac351
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-10-31 22:04:11 -0300

    enemy spawner en proceso

---
+++ Networking/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+    public GameObject prefab;
+    public int numberOfEnemies;
+
+    public override void OnStartServer()
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            var SpawnPosition = new Vector3(8, 8, 0);
+            var SpawnRotation = new Quaternion(8, 8, 0, 1);
+        }
+    }
+}
