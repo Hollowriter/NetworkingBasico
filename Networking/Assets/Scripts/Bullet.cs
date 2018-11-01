95008e43f13fdc22c7b157edeae7f0250a916e27
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-10-31 21:16:41 -0300

    pequeño avance con la vida que funciona

--- Networking/Assets/Scripts/Bullet.cs
+++ Networking/Assets/Scripts/Bullet.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("ping");
+        Debug.Log(collision.ToString());
+        Debug.Log(collision.gameObject.name);
+        Debug.Log("-x-");
         var hit = collision.gameObject;
-        var health = hit.GetComponent<Health>();
+        var health = hit.GetComponentInParent<Health>();
         if (health != null)
         {
             health.TakeDamage(10);
+            Debug.Log("destroying bullet");
         }
         Destroy(gameObject);
     }
--- Networking/Assets/Scripts/Health.cs
+++ Networking/Assets/Scripts/Health.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
     public const int maxHealth = 100;
+	[SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+	public RectTransform healthBar;
     public void TakeDamage(int amount)
     {
+		if(!isServer) // Esto se ejecuta solo si esta en server para que no genere cambios anomalos entre los clientes
+		{
+			return; // Se utiliza para que todos los valores de la vida tengan el mismo valor por mas que haya lag
+		}
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Debug.Log("Esta Merto");
         }
+        Debug.Log("Health: " + currentHealth);
+		// healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
     }
 }
